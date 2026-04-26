e3fac44b01c71315b10f14a43f8131e76cb09af6
zarifecoban <zarife.coban@bil.omu.edu.tr>
2026-04-27 01:01:23 +0300

    Create alan_hesaplama.c

---
+++ alan_hesaplama.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+    float boy,en,cevre,alan;
+
+
+    printf("dıktortgenın uzunlugunu gırınız : ");
+    scanf("%f", &boy);
+    printf("dıkdortgenın genıslıgını gırınız: ");
+    scanf("%f", &en);
+
+    cevre=2*(boy+en);
+    alan= boy*en;
+    printf("dıkdortgenın cevresı = %f\n bırımdır",cevre);
+    printf("dıkdortgenın alanı = %f bırım karedir", alan);
+
+}
