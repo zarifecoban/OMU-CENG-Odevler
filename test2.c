6473d2871adb9e28e3876c135de2929d3045e931
zarifecoban <zarife.coban@bil.omu.edu.tr>
2026-04-27 00:57:32 +0300

    Create test2.c

---
+++ test2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main (){
+    int i = 8;
+    printf("%d\n",i);
+
+
+    i += 5; // i = i +5
+    printf("%d\n",i);
+
+    i -= 5; // i = i -5
+    printf("%d\n",i);
+
+    i *= 5; // İ = i * 5
+    printf("%d\n",i);
+
+    i /= 4; // i = i/5
+    printf("%d\n",i);
+
+
+
+
+
+
+    int a;
+    a = 4;
+    printf("%d\n",a);
+    printf("%d\n",a++);//önce mevcut deger yazılacak sonra deger artıralacak
+    printf("%d\n",a);
+    printf("%d\n",++a);// önce deger artıralacak sonra yazdırılacak
+    printf("%d\n",a);
+
+
+    return 0;
+}
+
